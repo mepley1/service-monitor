835c6d59d32423c7df621ca9cf750557010078b1
Mike <47926856+mepley1@users.noreply.github.com>
2023-09-13 20:53:54 -0500

    Send email alert if service has failed
    Send email alert if monitored service has failed.
    Take service unit as command line arg

--- service-monitor.sh
+++ service-monitor.sh
@@ -2,12 +2,15 @@
 # Check if a service is active, if not then restart it and send alert.
 # Run this script as a cron job every 5-10 minutes.
 
-service_name="vmtoolsd"
+# Read name of service unit from cl arg
+service_name=$1
+alert_email=mike@localhost
 
-if ! systemctl is-active --quiet $service_name; then
-#  echo "$service_name is running."
-#else
+if systemctl is-active --quiet $service_name; then
+  echo "$service_name is running."
+else
   date +"%D %T $service_name is down, restarting..." >> ~/$service_name-restart-log.txt
   systemctl restart $service_name
-  date +"%D %T $service_name is down. Attempting to restart it, but you may still want to check the service."|mail -s "Alert: $service_name is down" alerts@deathmetal.me
+  date +"%D %T $service_name is down. Attempting to restart it, but you may still want to check the service."|mail -s "Alert: $service_name is down" $alert_email
 fi
+exit 0
